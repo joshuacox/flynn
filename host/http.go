89fa53dc20897cf8bb92ce82465b2f6f52b15c91
Corey Powell <mistdragon100@gmail.com>
2014-12-11 21:42:40 -0500

    host: (rbm) started on host/http.go
    Signed-off-by: Corey Powell <mistdragon100@gmail.com>

--- host/http.go
+++ host/http.go
@@ -6,80 +6,95 @@ import (
 	"net/http"
 
 	"github.com/flynn/flynn/Godeps/_workspace/src/github.com/julienschmidt/httprouter"
+	"github.com/flynn/flynn/host/types"
+	"github.com/flynn/flynn/pkg/rpcplus"
+	rpc "github.com/flynn/flynn/pkg/rpcplus/comborpc"
+	"github.com/flynn/flynn/pkg/shutdown"
 )
 
-func ListHosts(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func serveHTTP(host *Host, attach *attachHandler, sh *shutdown.Handler) error {
+	if err := rpc.Register(host); err != nil {
+		return err
+	}
+	rpc.HandleHTTP()
+	http.Handle("/attach", attach)
+
+	l, err := net.Listen("tcp", ":1113")
+	if err != nil {
+		return err
+	}
+	sh.BeforeExit(func() { l.Close() })
+	go http.Serve(l, nil)
+	return nil
+}
+
+type Host struct {
+	state   *State
+	backend Backend
+}
+
+func (h *Host) ListJobs(arg struct{}, res *map[string]host.ActiveJob) error {
+	*res = h.state.Get()
+	return nil
+}
+
+func (h *Host) GetJob(id string, res *host.ActiveJob) error {
+	job := h.state.GetJob(id)
+	if job != nil {
+		*res = *job
+	}
+	return nil
+}
+
+func (h *Host) StopJob(id string, res *struct{}) error {
+	job := h.state.GetJob(id)
+	if job == nil {
+		return errors.New("host: unknown job")
+	}
+	switch job.Status {
+	case host.StatusStarting:
+		h.state.SetForceStop(id)
+		return nil
+	case host.StatusRunning:
+		return h.backend.Stop(id)
+	default:
+		return errors.New("host: job is already stopped")
+	}
+}
+
+func (h *Host) StreamEvents(id string, stream rpcplus.Stream) error {
+	ch := h.state.AddListener(id)
+	defer h.state.RemoveListener(id, ch)
+	for {
+		select {
+		case event := <-ch:
+			select {
+			case stream.Send <- event:
+			case <-stream.Error:
+				return nil
+			}
+		case <-stream.Error:
+			return nil
+		}
+	}
+}
+
+func listJobs(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// || -- Accept: text/event-stream
+}
+
+func getJob(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+}
+
+func stopJob(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
 }
 
 func newRouter() {
 	r := httprouter.New()
-	/// Sampi
-	// hosts
-	r.GET("/cluster/hosts", ListHosts)
-	r.PUT("/cluster/hosts/:id", RegisterHost)
-	// jobs
-	r.POST("/cluster/jobs", AddJobs)
-	r.DLEETE("/cluster/jobs/:id", RemoveJob)
-	// events -- Accept: text/event-stream
-	r.GET("/cluster/events", StreamHostEvents)
 
-	/// Host
-	// || -- Accept: text/event-stream
-	r.GET("/host/jobs", ListJobs)
-	r.GET("/host/jobs/:id", GetJob)
-	r.DELETE("/host/jobs/:id", StopJob)
+	r.GET("/host/jobs", listJobs)
+	r.GET("/host/jobs/:id", getJob)
+	r.DELETE("/host/jobs/:id", stopJob)
 }
-
-/*
-# Sampi
-
-## ListHosts
-
-GET /cluster/hosts
-
-
-## AddJobs
-
-POST /cluster/jobs
-
-
-## RegisterHost
-
-PUT /cluster/hosts/:id
-
-
-## RemoveJob
-
-DELETE /cluster/jobs/:id
-
-
-## StreamHostEvents
-
-GET /cluster/events
-Accept: text/event-stream
-
-
-# Host
-
-## ListJobs
-
-GET /host/jobs
-
-
-## GetJob
-
-GET /host/jobs/:id
-
-
-## StopJob
-
-DELETE /host/jobs/:id
-
-
-## StreamEvents
-
-GET /host/jobs
-Accept: text/event-stream
-
-*/
