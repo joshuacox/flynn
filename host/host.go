44722bd52642eb513a5030c4a74123396834061a
Blaž Hrastnik <blaz.hrast@gmail.com>
2014-12-15 14:26:06 +0900

    register Sampi routes
    Signed-off-by: Blaž Hrastnik <blaz.hrast@gmail.com>

--- host/host.go
+++ host/host.go
@@ -17,7 +17,6 @@ import (
 	"github.com/flynn/flynn/host/types"
 	"github.com/flynn/flynn/pkg/attempt"
 	"github.com/flynn/flynn/pkg/cluster"
-	rpc "github.com/flynn/flynn/pkg/rpcplus/comborpc"
 	"github.com/flynn/flynn/pkg/shutdown"
 )
 
@@ -221,15 +220,15 @@ func runDaemon(args *docopt.Args) {
 	select {
 	case <-sampiStandby:
 		g.Log(grohl.Data{"at": "sampi_leader"})
-		rpc.Register(sampiCluster)
+		sampiCluster.ServeHTTP(sh)
 	case <-time.After(5 * time.Millisecond):
 		go func() {
 			<-sampiStandby
 			g.Log(grohl.Data{"at": "sampi_leader"})
-			rpc.Register(sampiCluster)
+			sampiCluster.ServeHTTP(sh)
 		}()
 	}
-	cluster, err := cluster.NewClientWithSelf(hostID, NewLocalClient(hostID, sampiCluster))
+	cluster, err := cluster.NewClientWithID(hostID)
 	if err != nil {
 		sh.Fatal(err)
 	}
